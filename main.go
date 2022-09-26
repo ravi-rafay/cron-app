a306120a5270ca5dcb9d93d1fb28e5bcec61b80d
ravi-rafay <ravi@rafay.co>
2022-09-26 16:53:18 +0530

    init

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type GlobalFunction func() Person
+
+func globalFuncImplementation() func() Person {
+	return func() Person {
+		return Person{
+			name: "Ravi Kumar",
+			born: time.Now(),
+		}
+	}
+}
+
+var (
+	gFunc GlobalFunction = globalFuncImplementation()
+)
+
+type Person struct {
+	name string
+	born time.Time
+}
+
+var getPeople = func() []Person {
+	var people []Person
+	for i := 0; i < 3; i++ {
+		people = append(people, gFunc())
+	}
+
+	return people
+}()
+
+func main() {
+
+	times := 0
+
+	for {
+
+		var people []Person
+		people = append(people, getPeople...)
+
+		for i := range people {
+			if times < 3 {
+				people[i].born = time.Now()
+			}
+		}
+
+		times += 1
+		for _, p := range people {
+			fmt.Println(p.born)
+		}
+
+		fmt.Println("=========")
+
+		time.Sleep(time.Second * 3)
+	}
+}
